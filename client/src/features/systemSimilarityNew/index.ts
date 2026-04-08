4bedc2d99be677f9d2e7142d00ab9b8ab20f3cb2
brihcherry <brihcherry@gmail.com>
2026-04-08 15:42:10 -0400

    Further pruning to remove elements of old reactors

--- client/src/features/systemSimilarityNew/index.ts
+++ client/src/features/systemSimilarityNew/index.ts
@@ -1,7 +1,6 @@
 export * from "./api/systemSimilarityApi-New";
 export * from "./components/HeatmapGrid-New";
 export * from "./components/RefreshHeatmapWidget-New";
-export * from "./hooks/useHeatmapData-New";
 export * from "./types";
 export * from "./utils/ComputePercentiles-New";
 export * from "./utils/formatRefreshPayload-New";
